488b0dc43ddca9391fb795f94e07987f004ed4f2
ControK <jorants@gmail.com>
2016-02-17 12:40:59 +0100

    added some comments

--- src/DDFS.c
+++ src/DDFS.c
@@ -2,6 +2,9 @@
 #include "DDFS.h"
 
 
+/*
+  This macro will add all predecesors of a nodes bud_star to the stack. 
+ */
 #define add_pred_to_stack(cur,stack)				\
   do{								\
     MVNodeP pred;						\
@@ -16,11 +19,17 @@
       }while(false);
 
 
-
+//gives the level of the node you would step into
 #define L(e) bud_star(e->n2)->min_level
+
+//shorthands to acces the stacks quickly, they are stored in the graph to avoid mallocs
 #define Sr g->red_stack
 #define Sg g->green_stack
 
+/*
+  This macro sets the below pointer of the current node to the next one and calculates the bud_star.
+  It does not yet step, as we might need to backtrack of have found  a petal
+ */
 #define prepare_next(Nx)			\
   if(Nx) {					\
     if(Nx->n1)					\
@@ -28,50 +37,64 @@
     Nx->n2 = bud_star(Nx->n2);			\
   }
 
-
-#define step_into(C,Nx,S)				\
-  prepare_next(Nx)					\
-  Nx->n2->above = Nx->n1;				\
-  C = Nx->n2;						\
-  C->visited = true;					\
-  C->ddfs_green = green_top;				\
-  C->ddfs_red = red_top;				\
-  add_to_list(result->nodes_seen,C);			\
-  add_pred_to_stack(C,S);				\
-  if(S.length > 0)					\
-    Nx = popp(S);					\
-  else\
+/*
+  Steps to the next node
+ */
+#define step_into(C,Nx,S)						\
+  prepare_next(Nx)              /* Set below pointer and calculate bud_star*/ \
+  Nx->n2->above = Nx->n1;						\
+  C = Nx->n2;                   /*Change center of DFS*/		\
+  C->visited = true;							\
+  C->ddfs_green = green_top;						\
+  C->ddfs_red = red_top;						\
+  add_to_list(result->nodes_seen,C);					\
+  add_pred_to_stack(C,S);      /*Add al unseen predecesors*/		\
+  if(S.length > 0)	       /*Check wether we hit the end of a backtrack*/ \
+    Nx = popp(S);							\
+  else									\
     Nx = NULL;
    
 
 
+
 int DDFS(MVGraph * g,MVNodeP green_top,MVNodeP red_top){
-  DDFS_result * result = &(g->last_ddfs);
+
   debug("--- DDFS(%i %i) --- \n",green_top->N,red_top->N);
+
+  //result is stored in the graph to avoid free's and mallocs
+  DDFS_result * result = &(g->last_ddfs);
+  //reset result
   result->nodes_seen.length = 0;
   result->bottleneck = NULL;
-  g->green_stack.length = 0;
-  g->red_stack.length = 0;
+
+  //reset stacks
+  Sr.length = 0;
+  Sg.length = 0;
+  //Start at the bridges, no center yet
   MVNodeP G = NULL;
   MVNodeP R = NULL;
-  
+
+
+  //steel memory from the stack to create the first next struct
   MVEdge * Ng;
   alloc_in_list((Sg),Ng);			
   Ng = popp(Sg);
-  Ng->n1 = NULL;
-  Ng->n2 = green_top;
+  Ng->n1 = NULL;  //there is no center yet
+  Ng->n2 = green_top; //here is where we want to go 
 
-  MVEdge * Nr;
+  MVEdge * Nr;  
   alloc_in_list((Sr),Nr);			
   Nr = popp(Sr);
   Nr->n1 = NULL;
   Nr->n2 = red_top;
 
+  //used to keep track later if we backtrack.
+  //if a backtrack fails then we need to jump back to the DDFS collision that started it
   MVEdge red_before,green_before;
   
-  
+
   if(bud_star(Nr->n2) == bud_star(Ng->n2)){
-    return DDFS_EMPTY;
+    return DDFS_EMPTY;   //avoid the whole loop thing
   }else if (Ng->n2->min_level == 0 && Nr->n2->min_level == 0){
     return DDFS_PATH; //return imidetly for one edge paths
   }
@@ -84,6 +107,7 @@ int DDFS(MVGraph * g,MVNodeP green_top,MVNodeP red_top){
   if(Ng->n2)
     debug(" %i\n",Ng->n2->N);
   
+
   
   while( R==NULL || G==NULL || //we are starting
 	 R->min_level >0 || G->min_level>0){    //we need some walking still
--- src/DDFS.h
+++ src/DDFS.h
@@ -11,5 +11,10 @@
 #define DDFS_OK 3
 #define DDFS_BOTTLENECK 4
 
+/*
+  This function will run a DDFS on green_top and red_top and store the result in g->ddfs_result;
+  It will return DDFS_EMPTY, DDFS_PETAL or DDFS_PATH depending on the outcome of the DDFS
+ */
 int DDFS(MVGraph * g,MVNodeP green_top,MVNodeP red_top);
+
 #endif
